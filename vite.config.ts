aa5685525ceb240a1cdf118ff97a41511fb9b9f6
Валерия <v.galenitskaya@list.ru>
2025-11-25 14:27:37 +0300

    Проект готов

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/interactive-line-chart/',
+  base: '/<Test_Interactive-Line-Chart>/',
 });
 
